88360ced67055096d44debd21f7e1a52b11c4f30
Mureigne Leivin Y. Daet <111488534+gfnijeno@users.noreply.github.com>
2022-09-08 14:12:11 +0800

    Create App.java

---
+++ exam_1/App.java
@@ -0,0 +1,19 @@
+public class App {
+    public static void main(String[] args){
+        Student mureigne = new Student();
+        
+        mureigne.surname = " Daet";
+        mureigne.firstName = "Mureigne Leivin ";
+        mureigne.middleInitial = " Y.";
+        mureigne.dateofBirth = "September 25, 2004";
+        mureigne.studentNumber = 2022104045;
+        mureigne.studentEmail = "daetmy@students.national-u.edu.ph";
+        mureigne.amIawesome = true;
+        mureigne.sayMyname();
+        mureigne.sayMybday();
+        mureigne.sayMystudent();
+        mureigne.sayMyawesomeness();
+
+    }
+}
+
